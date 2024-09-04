ed7617a4c789c8a0c6e9f043228019872567d7f5
Ariet <badanovariet55@gmail.com>
2024-09-04 20:22:21 +0300

    add dealer

---
+++ dealer.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.arrow')
+let nonCon = document.querySelector('.none-content')
+img.onclick =  function (){
+     nonCon.style.display = nonCon.style.display ==='block' ? 'none':'block '
+//     if(nonCon.style.display =='block'){
+//  img.onclick = function(){
+//         nonCon.style.display ='none'}
+// }
+}
